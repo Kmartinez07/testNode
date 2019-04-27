0a2a11c6da8a8c584f7ebae2c8e2c79abcf464dc
unknown <>
2019-04-26 22:15:48 -0500

    gitignore updated

--- api/routes.js
+++ api/routes.js
@@ -5,7 +5,7 @@ const imageController = require('./image.controller');
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, './public/images/');
+    cb(null, './public/images');
   },
   filename: function(req, file, cb) {
     cb(null, Date.now() + file.originalname);
